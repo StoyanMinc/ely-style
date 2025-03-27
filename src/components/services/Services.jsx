7f57ebe2023806553100720ec464c3f6a4b3b21e
Stoyan Minchev <154098583+StoyanMinc@users.noreply.github.com>
2025-03-27 08:14:52 +0200

    Create services page

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,11 +1,20 @@
+import { Link } from "react-router-dom"
+
 export default function Services() {
-    return( 
+    return (
         <div className="services-container">
             <section className="services-nav">
-                <div className="services-nav-holder"></div>
-                <div className="services-nav-holder"></div>
-                <div className="services-nav-holder"></div>
-                <div className="services-nav-holder"></div>
+                <div className="services-nav-holder service-manicure"><Link className="service-title">Manicures</Link></div>
+                <div className="services-nav-holder service-pedicure"><Link className="service-title">Pedicures</Link></div>
+                <div className="services-nav-holder service-manicure-art"><Link className="service-title">Nails Art <span className="ampersand"></span> Desings</Link></div>
+                <div className="services-nav-holder service-beauty"><Link className="service-title">Beauty</Link></div>
+            </section>
+            <section className="services-comment">
+                <div className="services-image-holder"></div>
+                <div className="services-comment-holder">
+                    <p>"Ely Style is hands down the best salon around, I definitily recommendet"</p>
+                    <span>- Stoyan M., Nail spa client</span>    
+                </div>
             </section>
         </div>
     )
